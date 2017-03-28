86da8a2e271aa5fdba099c17a72d9496a7f0d6e6
Mustafa Dokumaci <mustafa@foraa.com>
2017-03-28 13:04:18 +0200

    stamp and shape a bit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 import redis from 'redis'
+import bstamp from 'stamp'
 
 export default struct => {
-  
+  struct.set({
+    redis: {
+      props: {
+        url: (s, u) => {
+          s.set({ client: redis.createClient({ url: u }) })
+        },
+        client: true
+      },
+      connected: false,
+      define: {
+        load: (context, path) => {
+          context = context || false
+          path = path || []
+        }
+      },
+      on: {
+        data: {
+          remove (val, stamp, t) {
+            if (val === null) {
+              t.set({ connected: false })
+              t.get('client').quit(error => {
+                if (error) {
+                  t.get('root').emit('error', error)
+                }
+              })
+            }
+          }
+        }
+      }
+    }
+  })
+  struct.inherits.set({
+    on: {
+      data: {
+        redis (val, stamp, t) {
+
+        }
+      }
+    }
+  })
 }
